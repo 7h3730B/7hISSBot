85bd623f8850e67c614f8d02b22fcb9668f715d8
7h3730B <teoschulze5@gmail.com>
2020-05-24 09:23:09 +0200

    removed System tap because it is false on glitch.com

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -2,17 +2,12 @@ const {
     version,
     dependencies
 } = require("../../package");
-const si = require("systeminformation");
-const os = require("os");
-
 module.exports.info = {
     name: "stats",
     description: "Shows you stats about the bot"
 };
 
 module.exports.run = async (client, message, args) => {
-    const cpu = await si.cpu();
-    const cpuLoad = await si.currentLoad();
     let dependend = "NodeJS Version: " + process.version + "\n";
     Object.entries(dependencies).forEach((r) => {
         dependend += `~ [${r[0]}](https://npmjs.org/package/${r[0]})${r[1]}\n`;
@@ -30,10 +25,6 @@ module.exports.run = async (client, message, args) => {
             {
                 name: "Discord Specs",
                 value: `Serving **${client.guilds.cache.size}** Guilds\n**${Math.round(client.ws.ping)}** ms Latency to the Discord API `
-            },
-            {
-                name: "System",
-                value: `Memory (binary): ${((os.totalmem() - os.freemem()) / 1.074e+9).toFixed(2)}GiB / ${(os.totalmem() / 1.074e+9).toFixed(2)}GiB\ncore(s): ${cpu.cores} at ${cpu.speed} GHz\nused: ${Math.round(cpuLoad.currentload)} %`
             },
             {
                 name: "Dependencies",
